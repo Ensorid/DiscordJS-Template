3301b247f6f1e15a8403612ced12495543009efd
Ensorid <182452094+Ensorid@users.noreply.github.com>
2024-11-21 11:40:24 +0100

    Fix lint problems

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -1,30 +1,30 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import { logInConsole } from '../../config.json';
+import * as fs from "fs";
+import * as path from "path";
+import { logInConsole } from "../../config.json";
 
 export enum level {
-    INFO = 'INFO',
-    WARN = 'WARN',
-    ERROR = 'ERROR',
-    DEBUG = 'DEBUG',
+    INFO = "INFO",
+    WARN = "WARN",
+    ERROR = "ERROR",
+    DEBUG = "DEBUG",
 }
 
 export function log(message: string, level: level): void {
-    const logDir = path.join('logs');
-    const date = new Date();
-    const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
-    const logFilePath = path.join(logDir, `${formattedDate}.log`);
+	const logDir = path.join("logs");
+	const date = new Date();
+	const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+	const logFilePath = path.join(logDir, `${formattedDate}.log`);
 
-    if (!fs.existsSync(logDir)) {
-        fs.mkdirSync(logDir);
-    }
+	if (!fs.existsSync(logDir)) {
+		fs.mkdirSync(logDir);
+	}
 
-    const timeStamp = date.toTimeString().split(' ')[0];
-    const logMessage = `${timeStamp} [${level}] ${message}`;
+	const timeStamp = date.toTimeString().split(" ")[0];
+	const logMessage = `${timeStamp} [${level}] ${message}`;
 
-    if (logInConsole) {
-        console.log(logMessage)
-    }
+	if (logInConsole) {
+		console.log(logMessage);
+	}
 
-    fs.appendFileSync(logFilePath, logMessage+'\n', { encoding: 'utf8' });
+	fs.appendFileSync(logFilePath, logMessage + "\n", { encoding: "utf8" });
 }
