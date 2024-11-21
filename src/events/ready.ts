60bc5cdf3f94b19b781dcd9a09daec0230baece6
Ensorid <182452094+Ensorid@users.noreply.github.com>
2024-11-21 11:40:07 +0100

    Fix lint problems

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,14 @@
-import { Events, Client } from "discord.js";
+import { Events, Client, ActivityType } from "discord.js";
+import { log, level } from "../utilities/logger";
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client: Client) {
-		console.log(`Ready! Logged in as ${client.user?.tag}`);
+		client.user?.setPresence({
+			activities: [{ name: "Bot in devlopment", type: ActivityType.Custom }],
+			status: "idle",
+		  });
+		  log(`Ready! Logged in as ${client.user?.tag}`, level.DEBUG);
 	},
 };
